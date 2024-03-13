5c02e49d9c823cbb10680e3c45d7febaff65a3e5
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-13 18:49:24 +0530

    data added to localStorage

--- addToCart.js
+++ addToCart.js
@@ -1,4 +1,14 @@
+import { getCartProductFromLS } from "./getCartProductFromLS";
+
 export const addToCart = (event , id, stock) => {
+    
+    let arrLocalStorageProduct = getCartProductFromLS();
+    
     const currentProdElem = document.querySelector(`#card${id}`);
-    console.log(currentProdElem)
+    let quantity = Number(currentProdElem.querySelector(".productQuantity").innerText);
+    let price = currentProdElem.querySelector(".productPrice").innerText;
+    price = Number(price.replace('₹','')*quantity);
+
+    arrLocalStorageProduct.push({id, quantity, price})
+    localStorage.setItem("cartProductLS", JSON.stringify(arrLocalStorageProduct))
 }
