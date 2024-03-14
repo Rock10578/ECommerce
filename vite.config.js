6ce47a98f72443831c2f068999d5326cf3aa71b4
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-15 01:51:24 +0530

    Issue in deployment

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        bill: resolve(__dirname, "bill.html"),
         about: resolve(__dirname, "about.html"),
         contact: resolve(__dirname, "contact.html"),
         products: resolve(__dirname, "products.html"),
         addToCart: resolve(__dirname, "addToCart.html"),
-        // Add more entry points for other HTML files as needed
       },
     },
   },
